a5974e6fe8480ec29f5b8a5c5daa7264e846dc37
Toxic254 <cyrus_g@outlook.com>
2020-10-28 14:35:27 +0300

    Initial Commit. Checked out the Zen Of Python.

---
+++ hello-version.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Copyright 2018-2020 GCN http://www.0101solutions.consulting/
+# The Zen of Python, by Tim Peters
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
+import this # Zen Of Python. Is a function that prints <<.
+import platform # Checks the platform Python is running on.
+
+# Python Code to check the Python Version Running.
+print('This is python version {}'.format(platform.python_version()))
---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# The above is a Shebang line or Hashbang. Its a calling pattern for Unix based systems.
+# The #! must be included hence hash-bang, first two bytes in the file. The Unix env path to find the Python Interpreter in the Machine.
+# Copyright 2018-2020 GCN http://www.0101solutions.consulting/
+
+print('Hello, World.')
